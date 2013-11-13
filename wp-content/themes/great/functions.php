d73585860338dafad1d95fc047f7db56a8710973
Jonatas Eduardo <jottaeblog@gmail.com>
2013-11-13 00:43:36 -0200

    Criação do metabox Map no posttype lugar

--- wp-content/themes/great/functions.php
+++ wp-content/themes/great/functions.php
@@ -37,6 +37,139 @@ if ( function_exists('add_theme_support') ) add_theme_support('automatic-feed-li
 		'before_title' => '<h3>',
 		'after_title' => '</h3>',
 	));
+
+
+/*-----------------------------------------------------------------------------------*/
+/*	Metabox de um mapa
+/*-----------------------------------------------------------------------------------*/
+
+
+function add_custom_meta_box() {
+    add_meta_box(
+		'mapa_meta_box', // $id
+		'Mapa', // $title 
+		'show_custom_meta_box', // $callback
+		'lugar', // $page
+		'normal', // $context
+		'high'); // $priority
+}
+add_action('add_meta_boxes', 'add_custom_meta_box');
+  
+
+
+// Criação das Field Array
+$prefix = 'custom_';
+$custom_meta_fields = array(
+	array(
+		'label'=> 'Text Input',
+		'desc'	=> 'A description for the field.',
+		'id'	=> $prefix.'text',
+		'type'	=> 'text'
+	),
+	array(
+		'label'=> 'Textarea',
+		'desc'	=> 'A description for the field.',
+		'id'	=> $prefix.'textarea',
+		'type'	=> 'textarea'
+	),
+	array(
+		'label'=> 'Checkbox Input',
+		'desc'	=> 'A description for the field.',
+		'id'	=> $prefix.'checkbox',
+		'type'	=> 'checkbox'
+	),
+	array(
+		'label'=> 'Select Box',
+		'desc'	=> 'A description for the field.',
+		'id'	=> $prefix.'select',
+		'type'	=> 'select',
+		'options' => array (
+			'one' => array (
+				'label' => 'Option One',
+				'value'	=> 'one'
+			),
+			'two' => array (
+				'label' => 'Option Two',
+				'value'	=> 'two'
+			),
+			'three' => array (
+				'label' => 'Option Three',
+				'value'	=> 'three'
+			)
+		)
+	)
+);
+
+
+
+
+
+
+// The Callback
+function show_custom_meta_box() {
+global $custom_meta_fields, $post;
+// Use nonce for verification
+echo '<input type="hidden" name="custom_meta_box_nonce" value="'.wp_create_nonce(basename(__FILE__)).'" />';
+	
+	// Begin the field table and loop
+	echo '<table class="form-table">';
+	foreach ($custom_meta_fields as $field) {
+		// get value of this field if it exists for this post
+		$meta = get_post_meta($post->ID, $field['id'], true);
+		// begin a table row with
+		echo '<tr>
+				<p class="label' . $field['id'] . $field['label'] .'">' . $field['label'] . '</p>';
+				switch($field['type']) {
+					// textarea
+					// text  
+					case 'text':  
+					    echo '<input type="text" name="'.$field['id'].'" id="'.$field['id'].'" value="'.$meta.'" size="30" /> 
+					        <br /><span class="description">'.$field['desc'].'</span>';  
+						break; 
+					case 'textarea':
+						echo '<textarea name="'.$field['id'].'" id="'.$field['id'].'" cols="60" rows="4">'.$meta.'</textarea>
+							<br /><span class="description">'.$field['desc'].'</span>';
+						break;
+				} //end switch
+		echo '</tr>';
+	} // end foreach
+	echo '</table>'; // end table
+}
+
+
+
+// Save the Data
+function save_custom_meta($post_id) {
+    global $custom_meta_fields;
+	
+	// verify nonce
+	if (!wp_verify_nonce($_POST['custom_meta_box_nonce'], basename(__FILE__))) 
+		return $post_id;
+	// check autosave
+	if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE)
+		return $post_id;
+	// check permissions
+	if ('page' == $_POST['post_type']) {
+		if (!current_user_can('edit_page', $post_id))
+			return $post_id;
+		} elseif (!current_user_can('edit_post', $post_id)) {
+			return $post_id;
+	}
+	
+	// loop through fields and save the data
+	foreach ($custom_meta_fields as $field) {
+		$old = get_post_meta($post_id, $field['id'], true);
+		$new = $_POST[$field['id']];
+		if ($new && $new != $old) {
+			update_post_meta($post_id, $field['id'], $new);
+		} elseif ('' == $new && $old) {
+			delete_post_meta($post_id, $field['id'], $old);
+		}
+	} // end foreach
+}
+add_action('save_post', 'save_custom_meta');  
+
+
 /*-----------------------------------------------------------------------------------*/
 /*	Load Widgets & Shortcodes
 /*-----------------------------------------------------------------------------------*/
