001570388c201f1c3dedd2e857634f2dbfa7bb21
Jonatas Eduardo <jottaeblog@gmail.com>
2013-11-14 15:02:17 -0200

    O js que controla o mapa na dashboard via jquery

--- js/map.js
+++ js/map.js
@@ -1,12 +1,101 @@
 $(document).ready(function(){
-	function initialize() {
-        var mapOptions = {
-          center: new google.maps.LatLng(-30.036384, -51.216524),
-          zoom: 13,
-          mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-        var map = new google.maps.Map(document.getElementById("map-canvas"),
-            mapOptions);
-      }
-      google.maps.event.addDomListener(window, 'load', initialize);
+
+		var geocoder = new google.maps.Geocoder();
+
+		$('#map-canvas').gmap3({
+			map: {
+				options: {
+					center: [-30.036363, -51.214786],
+					zoom: 13,
+					mapTypeId: google.maps.MapTypeId.ROADMAP,
+					mapTypeControl: true,
+					mapTypeControlOptions: {
+						style: google.maps.MapTypeControlStyle.DROPDOWN_MENU
+					},
+					navigationControl: true,
+					scrollwheel: true,
+					streetViewControl: true
+				}
+			},
+			marker: {
+				latLng: [-30.036363, -51.214786],
+				options: {
+					draggable: true
+				},
+				events: {
+					dragend: function(marker) {
+						$(this).gmap3({
+							getaddress: {
+								latLng: marker.getPosition(),
+								callback: function(results) {
+									var map = $(this).gmap3("get"),
+										infowindow = $(this).gmap3({
+											get: "infowindow"
+										}),
+										content = results && results[1] ? results && results[0].formatted_address : "no address";
+										contentCut = content.split(', Porto Alegre')[0];
+									if (infowindow) {
+										infowindow.open(map, marker);
+										infowindow.setContent(contentCut);
+									} else {
+										$(this).gmap3({
+											infowindow: {
+												anchor: marker,
+												options: {
+													content: content
+												}
+											}
+										});
+									}
+								}
+							}
+						});
+					},
+					drag: function() {
+						updateMarkerStatus('Dragging...');
+					}
+
+				}
+			}
+		});
+
+		var map = $(this).gmap3("get");
+		
+
+		function geocodePosition(pos) {
+		  geocoder.geocode({
+		    latLng: pos
+		  }, function(responses) {
+		    if (responses && responses.length > 0) {
+		      updateMarkerAddress(responses[0].formatted_address);
+		    } else {
+		      updateMarkerAddress('Cannot determine address at this location.');
+		    }
+		  });
+		}
+
+		function updateMarkerStatus(str) {
+		  document.getElementById('markerStatus').innerHTML = str;
+		}
+
+		function updateMarkerPosition(latLng) {
+		  	$("#info").text(latLng);
+		
+		
+		}
+
+		function updateMarkerAddress(str) {
+		  $("address").text(str);
+		}
+
+
+		  // Update current position info.
+		  updateMarkerPosition(latLng);
+		  geocodePosition(latLng);
+		  
+	
+
+		
+	
+
   });
