86f0d727e46eed321104043c68e88e60ebf4b196
Jonatas Eduardo <jottaeblog@gmail.com>
2013-11-16 16:14:09 -0200

    map-dashboard criado para controlar o mapa backend

---
+++ js/map-dashboard.js
@@ -0,0 +1,176 @@
+$(document).ready(function(){
+
+
+		// Adiciona o map-canvas abaixo do input buscador
+		$("<div id='map-canvas'></div>").insertAfter("#acf-endereço");
+		$("#acf-endereço").css({"width":"100%"});
+ 
+		// Adiciona o botao abaixo do mapa
+		$("<div id='botao'><span>Buscar endereço</span></div>").insertAfter("#acf-endereço");
+		$("#botao").css({"width":"100%"});
+
+		// chama o geocoder do Google
+		var geocoder = new google.maps.Geocoder();
+
+		// Efetua o corte de parte da string retornada do geocoder - endereço
+		function contains(str, text) {
+  		 	return (str.indexOf(text) >= 0);
+		}
+
+		// Criação do mapa
+		$('#map-canvas').gmap3({
+			map: {
+				options: {
+					center: [-30.036363, -51.214786],
+					zoom: 13,
+					mapTypeId: google.maps.MapTypeId.ROADMAP,
+					mapTypeControl: true,
+					mapTypeControlOptions: {
+						style: google.maps.MapTypeControlStyle.DROPDOWN_MENU
+					},
+					navigationControl: true,
+					scrollwheel: true,
+					streetViewControl: true
+				}
+			},
+			marker: {
+				latLng: [-30.036363, -51.214786],
+				options: {
+					draggable: true
+				},
+				events: { 
+					dragend: function(marker) {
+						$(this).gmap3({
+							getaddress: {
+								latLng: marker.getPosition(),
+								callback: function(results) {
+									var map = $(this).gmap3("get"),
+									infowindow = $(this).gmap3({
+										get: "infowindow"
+									}),
+									
+									content = results && results[0] ? results && results[0].formatted_address : "no address";
+
+									// Corta a string do endereco se tiver a palavra Porto Alegre
+									if(contains(content, 'Porto Alegre')) {
+										contentEcho = content.split(', Porto Alegre')[0];
+									}else{
+										contentEcho = content;
+									}
+
+									// Exibe o endereco recortado na barra de busca
+									$('#acf-field-endereço').val(contentEcho);
+
+
+									// Cria o balão de informação com o endereço do marcador - dragend	
+									if (infowindow) {
+										infowindow.open(map, marker);
+										infowindow.setContent(contentEcho);
+									} else {
+										$(this).gmap3({
+											infowindow: {
+												anchor: marker,
+												options: {
+													content: contentEcho
+												}
+											}
+										});
+									}
+								}
+							},
+						});
+						
+						var latlng = marker.getPosition();
+						$('#acf-field-latlong').val(latlng);
+
+					}
+				}
+			}
+		});
+
+		var map = $(this).gmap3("get");
+		
+		function buscaLatlong(endereco) {
+			$("#map-canvas").gmap3({
+				clear: {
+					name: "marker"
+				},
+				getlatlng: {
+					address: endereco,
+					callback: function(results) {
+						if (!results) return;
+						$(this).gmap3({
+							marker: {
+								latLng: results[0].geometry.location,
+								options:{
+									draggable:true
+								},
+								events: {
+									dragend: function(marker) {
+										$(this).gmap3({
+											getaddress: {
+												latLng: marker.getPosition(),
+												callback: function(results) {
+													var map = $(this).gmap3("get"),
+													infowindow = $(this).gmap3({
+														get: "infowindow"
+													}),
+													
+													content = results && results[0] ? results && results[0].formatted_address : "Endereço não encontrado =(";
+
+													if(contains(content, 'Porto Alegre')) {
+														contentEcho = content.split(', Porto Alegre')[0];
+													}else{
+														contentEcho = content;
+													}
+
+
+													$('#acf-field-endereço').val(contentEcho);
+
+
+														
+													if (infowindow) {
+														infowindow.open(map, marker);
+														infowindow.setContent(contentEcho);
+													} else {
+														$(this).gmap3({
+															infowindow: {
+																anchor: marker,
+																options: {
+																	content: contentEcho
+																}
+															}
+														});
+													}
+
+													var latlng = marker.getPosition();
+													$('#acf-field-latlong').val(latlng);
+												}
+											},
+										});
+									}
+								}
+							}
+						});
+						var map = $(this).gmap3("get");
+						var latLng = results[0].geometry.location; //Makes a latlng
+      					map.panTo(latLng); //Make map global
+
+      					
+						$('#acf-field-latlong').val(latLng);
+
+					}
+				},
+			});
+		}
+	
+		// Botao para chamar o endereço no mapa
+		$("#botao").click(function(){
+			var endereco = $("#acf-field-endereço").val();
+			buscaLatlong(endereco);
+		});
+	
+	
+	
+
+  });
