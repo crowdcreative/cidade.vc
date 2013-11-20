9a868ad762098a2d081f8b3783a14b0dc5001f58
Jonatas Eduardo <jottaeblog@gmail.com>
2013-11-20 02:54:19 -0200

    Footer adicionado ao template

--- wp-content/themes/great/footer.php
+++ wp-content/themes/great/footer.php
@@ -2,23 +2,20 @@
 	</div><!--#page-->
 </div><!--.container-->
 </div>
+
+
+
+
 	<footer>
-		<div class="footer-social">
-			<div class="footer-social-inner">
-				<div class="footer-inner">
-					<?php mts_top_social_buttons(); ?>			
-				</div>
-			</div>
-		</div>
 		<div class="container">
-			<div class="footer-widgets">
-				<?php widgetized_footer(); ?>
-               			<div class="copyrights">
-					<?php mts_copyrights_credit(); ?>
-				</div> 
-			</div><!--.footer-widgets-->
-		</div><!--.container-->
+			<span><a href="http://cidadevc.tumblr.com/">http://cidadevc.tumblr.com</a> / <a href="https://github.com/crowdcreative/cidade.vc">GitHub</a> / Copyleft e Opensource. <3</span>
+		</div>
 	</footer><!--footer-->
+
+
+
+
+
 <?php mts_footer(); ?>
 <?php wp_footer(); ?>
 </body>
